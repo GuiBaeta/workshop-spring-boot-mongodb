4fb80fa7679bdfdbd4751cf9663f12ddbfc075f8
guibaeta <guilhermebaetapena@gmail.com>
2022-08-28 18:56:24 -0300

    Obtendo um post por id

---
+++ src/main/java/com/guibaeta/workshopmongo/services/PostService.java
@@ -0,0 +1,23 @@
+package com.guibaeta.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.guibaeta.workshopmongo.domain.Post;
+import com.guibaeta.workshopmongo.repository.PostRepository;
+import com.guibaeta.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+	
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+		}
+	
+}
